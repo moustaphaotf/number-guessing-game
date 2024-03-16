1e1aa9e9d27e7ed6e202831dc5257ddcdfb6df5e
Mamadou Moustapha  Diallo <moustaphaotf@gmail.com>
2024-03-16 03:59:49 +0000

    feat: add project setup and overview

--- number_guess.sh
+++ number_guess.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=number_guessing_game -t --no-align -c"
 echo -e "\n~~Number Guessing Game~~\n"
+
+echo -e "\nEnter your username:"
+read USERNAME
+
+# if not input
+if [[ -z $USERNAME ]]
+  echo -e "\nYou should enter your username !"
+  exit
+fi
+
+# look for user
+# if user exist
+# go for the game
+# if not found
+# insert user
+# get new user name
+# go for the game
