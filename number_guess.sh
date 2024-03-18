a92731d4aed1fe5290fcf5e5cc625cd271c3cc7c
Mamadou Moustapha  Diallo <moustaphaotf@gmail.com>
2024-03-18 08:08:21 +0000

    fix: on before game logic updated

--- number_guess.sh
+++ number_guess.sh
@@ -7,14 +7,30 @@ read USERNAME
 
 # if not input
 if [[ -z $USERNAME ]]
-  echo -e "\nYou should enter your username !"
+then
+  echo -e "You should enter your username !"
   exit
 fi
 
 # look for user
-# if user exist
-# go for the game
-# if not found
-# insert user
-# get new user name
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username ILIKE '$USERNAME'")
+# if user do not exist exist
+if [[ -z $USER_ID ]]
+then  
+  # insert user
+  INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+  # get new user id
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE username ILIKE '$USERNAME'")
+fi
+
+# get user games info
+GAMES_INFOS=$($PSQL "SELECT COUNT(*) total_games, MIN(guesses) best_game FROM games WHERE user_id=$USER_ID GROUP BY user_id")
+if [[ -z $GAME_INFOS ]]
+then
+  echo -e "Welcome, $USERNAME! It looks like this is your first time here."
+else
+  echo $GAMES_INFOS | read TOTAL_GAMES BEST_GAME
+  echo -e "\nWelcome back, $USERNAME! You have played $TOTAL_GAMES games, and your best game took $BEST_GAME guesses."
+fi
+
 # go for the game
